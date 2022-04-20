a1810ea28230486f997e3186ac6084e5b5ac437a
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-20 15:30:36 -0400

    commit tree 6d26bc2d64886d6ce9ba8cee8606324742032b33

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,99 @@
+
+class TicTacToe 
+
+    WIN_COMBINATIONS = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [6, 4, 2]
+    ]
+
+    def initialize()
+        @board = [" "," "," "," "," "," "," "," "," "]
+    end
+
+    def display_board 
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts  "--------------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "--------------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end 
+    
+    def input_to_index(user_input)
+       convert_input = user_input.to_i
+       convert_input -= 1
+    end 
+
+    def move(index, token = "X")
+        @board[index] = token 
+    end
+
+    def position_taken?(index)
+        @board[index] != " "
+    end
+
+    def valid_move?(index)
+        !position_taken?(index) && index >=0 && index <= 8
+    end
+
+    def turn_count
+        @board.count{|cell| cell != " "} 
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn
+        puts "Make your move (1-9)"
+        user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+   end
+
+   def won?
+     WIN_COMBINATIONS.any? do |combo|
+          if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+            return combo
+          end
+      end
+    end
+
+    def full?
+        @board.all?{|cell| cell != " "}
+    end
+
+    def draw?
+        full? && !won? 
+    end
+
+    def over?
+        draw? || won?  
+    end 
+
+    def winner
+        if won? 
+            return current_player === "X" ? "O" : "X"
+        end
+    end
+
+    def play
+        turn until over?
+            if won? 
+                puts "Congratulations #{winner}!"
+            else draw? 
+                puts "Cat's Game!"
+            end
+    end
+end
